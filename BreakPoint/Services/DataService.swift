2d2641053f11fb157ff1712bd29b0001c96e76bf
Jukess <juancaluque@hotmail.es>
2020-01-30 08:20:51 -0600

    Building the siginVC

---
+++ BreakPoint/Services/DataService.swift
@@ -0,0 +1,41 @@
+//
+//  DataService.swift
+//  BreakPoint
+//
+//  Created by Juan Luque on 1/29/20.
+//  Copyright © 2020 Juan Luque. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instace = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBUser(uid: String, userData: Dictionary<String, Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+    
+}
